614f102f4cbdf46f855aa39fffc099ae15d250c1
Vitalii Honchar <weaxme@gmail.com>
2025-06-26 07:57:56 +0200

    Added reddit lib

--- cmd/redditanalyzer/main.go
+++ cmd/redditanalyzer/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"context"
+
 	"github.com/sirupsen/logrus"
+	"github.com/vartanbeno/go-reddit/v2/reddit"
 )
 
 func main() {
 	log := logrus.New()
-	log.SetFormatter(&logrus.TextFormatter{
-		ForceColors:     true,
-		FullTimestamp:   true,
+	log.SetFormatter(&logrus.JSONFormatter{
+		PrettyPrint: true,
 		TimestampFormat: "2006-01-02 15:04:05",
 	})
-	log.Info("Hello world log")
+
+	ctx := context.Background()
+	redditClient := reddit.DefaultClient()
+	posts, _, err := redditClient.Subreddit.TopPosts(ctx, "golang", &reddit.ListPostOptions{
+		ListOptions: reddit.ListOptions{
+			Limit: 10,
+		},
+		Time: "day",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// for _, post := range posts {
+	// 	log.WithField("title", post.Title).
+	// 		WithField("id", post.ID).
+	// 		WithField("score", post.Score).
+	// 		WithField("created", post.Created).
+	// 		Info("Post details")
+	// }
+
+	post, _, err := redditClient.Post.Get(ctx, posts[0].ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+
+	log.WithField("post", post).Info("Post details fetched by ID")
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,16 @@ module reddit-analyzer
 
 go 1.24.4
 
-require github.com/sirupsen/logrus v1.9.3
+require (
+	github.com/sirupsen/logrus v1.9.3
+	github.com/vartanbeno/go-reddit/v2 v2.0.1
+)
 
-require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+require (
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20190108225652-1e06a53dbb7e // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+)
